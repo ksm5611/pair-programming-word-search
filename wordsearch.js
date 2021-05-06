15c512a9a89978327b3bf9b49f607e5d6fcdf050
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-05-06 00:53:12 +0000

    pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const verticalJoin = [];
+
+  if (letters.length === 0) {
+    return false;
+  }
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  for (let character of letters) {
+    for (let char = 0; char < character.length; char++) {
+      verticalJoin.push(character[char]);
+    }
+  }
+
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+
+};
+
+module.exports = wordSearch;
